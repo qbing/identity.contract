1af7509bed45f2f9f00ab4ae15a6da417d5d5aeb
qbing <q.bryant@live.com>
2017-10-20 18:45:59 +0800

    update idfactory

--- contracts/IdentityFactory.sol
+++ contracts/IdentityFactory.sol
@@ -3,6 +3,7 @@ import "./RecoveryQuorum.sol";
 
 
 contract IdentityFactory {
+    mapping(address => address) public senderToProxy;
     event IdentityCreated(
         address indexed userKey,
         address proxy,
@@ -12,6 +13,7 @@ contract IdentityFactory {
 
     //cost ~2.4M gas
     function CreateProxyWithControllerAndRecovery(address userKey, address[] delegates, uint longTimeLock, uint shortTimeLock) {
+        require(senderToProxy[userKey] == address(0x0));
         Proxy proxy = new Proxy();
         RecoverableController controller = new RecoverableController(proxy, userKey, longTimeLock, shortTimeLock);
         proxy.transfer(controller);
@@ -19,5 +21,6 @@ contract IdentityFactory {
         controller.changeRecoveryFromRecovery(recoveryQuorum);
 
         IdentityCreated(userKey, proxy, controller, recoveryQuorum);
+        senderToProxy[userKey] = proxy;
     }
 }
